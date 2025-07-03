3c07a0e46c41b253c36f72baa7837017e9b6dcb0
tanishka-taras <tanishkataras30@gmail.com>
2025-07-03 13:50:34 +0000

    3\07

---
+++ arry.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i=0;
+    int marks[6];
+
+    marks[0]=90;
+    marks[1]=70;
+    marks[2]=30;
+    marks[3]=76;
+    marks[4]=83;
+
+    for(i=0;i<5;i++){
+    printf("%d\n",marks[i]);
+    }
+
+
+}
