0074fde1fd1056830e68a4883f6e8f27de1c03e8
tanishka-taras <tanishkataras30@gmail.com>
2025-07-07 14:25:41 +0000

    07\7

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,24 +1,14 @@
 #include<stdio.h>
-void change(){
-    int arr[4][4],i,j;
-    for(i=0;i<4;i++)
+int main(){
+    int arry[10]={1,2,3,4,5,6,7,8,9,10};
+    int sum=0,i;
+
+    for(i=0;i<=10;i++)
     {
-        for(j=0;j<4;j++)
-        {
-            printf("enter a[%d][%d]",i,j);
-            scanf("%d",&arr[i][j]);
-        }
+        sum+=i;
+    printf("sum of the elements%d\n",sum);
+
     }
-      
- printf("printing a element \n");
- for(i=0;i<4;i++)
- {
-    printf("\n");
-    for(j=0;j<4;j++)
-    {
-        printf("%d\t",arr[i][j]);
-    }
-}
-}      
-        
+    printf("sum of the elements%d\n",sum);
     
+}
