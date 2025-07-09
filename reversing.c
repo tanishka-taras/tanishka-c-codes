89e461406e3012ff247b30a362784e67765cd492
tanishka-taras <tanishkataras30@gmail.com>
2025-07-09 14:32:38 +0000

    pailndrom.c

--- reversing.c
+++ reversing.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 int main(){
-    int num,r,reversed=0;
+    int num,n,r,reversed=0;
     printf("enter the number");
     scanf("%d",&num);
     
-
+n=num;
     while(num>0){
         r=num%10;
         reversed=reversed*10+r;
         num=num/10;
     }
     printf("reversed the number%d\n",reversed);
+    if(n==reversed)
+    printf("it is palindrome");
+    else
+    printf("it is not palindrome");
 }
        
 
