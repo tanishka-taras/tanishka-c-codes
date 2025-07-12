acc8d711ccfe25e09c79737887b74b4b16e7ef0f
tanishka-taras <tanishkataras30@gmail.com>
2025-07-12 14:14:15 +0000

    12\07

---
+++ dynamic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n,i,*p,sum=0;
+    printf("enter a number of elements");
+    scanf("%d\n",&n);
+
+    p=(int*)malloc(n*sizeof(int));
+    if(p==NULL)
+    {
+        printf("unable to allocate memory");
+    }
+    printf("enter of elements arry");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d\n",p+i);
+        sum=+*(p+i);
+    }
+    printf("sum=%d",sum);
+    free(p);
+}
