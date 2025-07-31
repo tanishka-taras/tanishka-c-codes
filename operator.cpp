f85cde3a8222e348dff3aedad6aac67246a524bf
tanishka-taras <tanishkataras30@gmail.com>
2025-07-31 13:47:51 +0000

    31/07

---
+++ operator.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=7,b=5;
+
+
+cout<<"arthimatic operator"<<endl;    
+cout<<"add"<<(a+b)<<endl;
+cout<<"sub"<<(a-b)<<endl;
+cout<<"mul"<<(a*b)<<endl;
+cout<<"div"<<(a/b)<<endl;
+cout<<"mod"<<(a%b)<<endl;
+cout<<"increment"<<(++a)<<endl;
+cout<<"decrement"<<(--a)<<endl;
+
+cout<<"relational operator"<<endl;
+cout<<"equal"<<(a==b)<<endl;
+cout<<"not equal"<<(a!=b)<<endl;
+cout<<"greater"<<(a>b)<<endl;
+cout<<"less"<<(a<b)<<endl;
+cout<<"greater than equal"<<(a>=b)<<endl;
+cout<<"less than equal"<<(a<=b)<<endl;
+
+
+cout<<"logical operat"<<endl;
+cout<<"and"<<(a&&b)<<endl;
+cout<<"or"<<(a||b)<<endl;
+cout<<"not"<<(!b)<<endl;
+
+
+cout<<"bitwise operator"<<endl;
+cout<<"and"<<(a&b)<<endl;
+cout<<"or"<<(a|b)<<endl;
+cout<<"not"<<(a^b)<<endl;
+cout<<"left shift"<<(a<<1)<<endl;
+cout<<"right shift"<<(a>>1)<<endl;
+
+
+cout<<"assignment operator"<<endl;
+cout<<"assig"<<(a)<<endl;
+cout<<"add assig"<<(a+=b)<<endl;
+cout<<"sub assig"<<(a-=b)<<endl;
+cout<<"mul assig"<<(a*=b)<<endl;
+cout<<"div assig"<<(a/=b)<<endl;
+cout<<"mod assig"<<(a%=b)<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
