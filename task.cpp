42338883585fa60ace19d23ab9c1575ee9852878
tanishka-taras <tanishkataras30@gmail.com>
2025-08-14 13:55:03 +0000

    intake from user

---
+++ task.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class student{
+
+public:
+int roll_no;
+string name;
+int marks;
+
+student(){
+    roll_no;
+    name;
+    marks;
+}
+};
+int main(){
+    int roll_no;
+    string name;
+    int marks;
+    cout<<"enter roll no: "<<endl;
+    cin>>roll_no;
+    cout<<"enter name: "<<endl;
+    cin>>name;
+    cout<<"enter marks: "<<endl;
+    cin>>marks;
+    return 0;
+}
+
+
